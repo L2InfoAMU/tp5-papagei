96b522b6d2783a278b57a1fb51bfb0bf3bdbae3d
a16002939 <is.abdelwahab@gmail.com>
2018-11-28 11:06:46 +0100

    creation de la classe de TACHE 2;  avec ses attributs;

---
+++ src/main/java/PaletteRasterImage.java
@@ -0,0 +1,18 @@
+
+import image.Image;
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+public class PaletteRasterImage implements Image {
+
+    private int width;
+    private int height;
+    private List<Color> palette;
+    private int[][] indexesOfColors;
+
+
+
+
+
+}
