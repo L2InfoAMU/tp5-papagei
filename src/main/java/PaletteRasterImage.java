f2677e4843352ce26d1f6b4c71705ddf8246a501
a16002939 <is.abdelwahab@gmail.com>
2018-11-28 11:17:20 +0100

    TACHE 2:   Creation 1er constructeur   et la methode getPixelColor   suite a l'importation du contrat de la classe;

--- src/main/java/PaletteRasterImage.java
+++ src/main/java/PaletteRasterImage.java
@@ -1,4 +1,6 @@
 
+// REMEMBER  x => columns AND y => ROWS
+
 import image.Image;
 import javafx.scene.paint.Color;
 
@@ -12,7 +14,39 @@ public class PaletteRasterImage implements Image {
     private int[][] indexesOfColors;
 
 
+    /************** CONSTRUCTOR *************************************************/
+    public PaletteRasterImage(Color color, int width, int height){
+        this.height = height;
+        this.width = width;
+        palette.set(0, color);
+        // all pixel have to have this index.
+        for(int column=0; column < width ; column++){
+            for (int row=0; row < height; row++){
+                indexesOfColors[column][row] = 0;
+            } // end for row
+        }// enf for column
+    }
+
+    public PaletteRasterImage(Color[][] pixels){
 
 
+    }
 
+    /************** METHODS *************************************************/
+    @Override
+    public Color getPixelColor(int x, int y) {
+        //het the index of the color in [x][y]:
+        int index = indexesOfColors[x][y];
+        return palette.get(index);
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
 }
