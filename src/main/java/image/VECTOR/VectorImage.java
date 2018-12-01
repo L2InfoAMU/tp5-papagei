6c474070140ff5ae346d035ef08149d87d897d7d
ismail <is.abdelwahab@gmail.com>
2018-12-02 00:42:31 +0100

    Tache 5:    -Coding the Vector image methods.    -importing the necessary features.

--- src/main/java/image/VECTOR/VectorImage.java
+++ src/main/java/image/VECTOR/VectorImage.java
@@ -1,22 +1,41 @@
 package image.VECTOR;
 
-import image.Image;
+import image.Point;
 import javafx.scene.paint.Color;
+import java.util.List;
+import image.Image;
+import image.Shape;
 
 public class VectorImage implements Image {
+    private List<Shape> shapes;
+    private int width;
+    private int height;
 
+    /******************** CONSTRUCTORS *******************************/
+    VectorImage(List<Shape> shapes, int width, int height){
+        this.shapes = shapes;
+        this.width = width;
+        this.height = height;
+    }
+
+    /************** GETTERS *************************************************/
     @Override
     public Color getPixelColor(int x, int y) {
-        return null;
+        Point myPoint = new Point(x,y);
+        for (Shape myShape : shapes){
+            if (myShape.contains(myPoint)){
+                return myShape.getColor();
+            }
+        }
+        return null; //we assume here that the point is in one of the shapes.
     }
-
     @Override
-    public int getWidth() {
-        return 0;
-    }
-
+    public int getWidth() { return this.width; }
     @Override
-    public int getHeight() {
-        return 0;
-    }
+    public int getHeight() { return this.height; }
+
+    /************** SETTERS *************************************************/
+    protected void setWidth(int width){this.width = width;}
+    protected void setHeight(int height){this.height = height;}
+
 }
